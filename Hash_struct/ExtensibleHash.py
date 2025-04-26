41ae55af6fb7229f7ff83d7bf832d6f164001fef
RodrigoLiC <rodrigo.li@utec.edu.pe>
2025-04-26 15:30:56 -0500

    RegistroType universal version 1

--- Hash_struct/ExtensibleHash.py
+++ Hash_struct/ExtensibleHash.py
@@ -33,44 +33,56 @@ def find_numbers_with_suffix(n, suffix):
             result.append(i)
     return result
 
-# TODO: Estructura temporal de registro (formato estatico)
 class RegistroType:
     """
     Estructura que maneja el control de los registros.
-    RT.size = espacio en bytes
-    RT.FORMAT = formato de los registros
-    RT.to_bytes = convierte el registro a bytes
-    RT.from_bytes = convierte bytes a un registro
-    RT.get_key = obtiene el valor del indice de ordenamiento del registro
+    size = espacio en bytes
+    FORMAT = formato de los registros (struct)
+    dict_format = formato de los registros (diccionario)
+    to_bytes = convierte el registro a bytes
+    from_bytes = convierte bytes a un registro
+    get_key = obtiene el valor del indice de ordenamiento del registro
     """
-    FORMAT = '3s20s2s'
-
-    def __init__(self, key):
-        self.key_index = key
+    def __init__(self, dict_format, keyIndex):
+        self.dict_format = dict_format
+        self.keyIndex = keyIndex
+        self.key = list(dict_format.keys())[keyIndex]
+        self.FORMAT = ''.join(dict_format.values())
         self.size = struct.calcsize(self.FORMAT)
 
-
-    def to_bytes(self, codigo, nombre, ciclo):
+    def to_bytes(self, args):
         """
         Convierte el registro a bytes.
         """
-        return struct.pack(self.FORMAT,
-                           codigo.encode('utf-8').ljust(3, b'\x00'),
-                           nombre.encode('utf-8').ljust(20, b'\x00'),
-                           ciclo.zfill(2).encode('utf-8')
-                           )
+        types = list(self.dict_format.values())
+        for i in range(len(args)):
+            if types[i] == 'i':
+                args[i] = int(args[i])
+            elif types[i] == 'f':
+                args[i] = float(args[i])
+            else:
+                args[i] = args[i].encode('utf-8').ljust(20, b'\x00')
+
+        return struct.pack(self.FORMAT, *args)
 
     def from_bytes(self, data):
         """
         Convierte bytes a un registro.
         """
-        unpacked = struct.unpack(self.FORMAT, data)
-        return [unpacked[0].decode('utf-8').strip('\x00'),
-                unpacked[1].decode('utf-8').strip('\x00'),
-                unpacked[2].decode('utf-8').strip('\x00')]
+        unpacked = list(struct.unpack(self.FORMAT, data))
+        types = list(self.dict_format.values())
+        for i in range(len(unpacked)):
+            if types[i] == 'i':
+                unpacked[i] = int(unpacked[i])
+            elif types[i] == 'f':
+                unpacked[i] = float(unpacked[i])
+            else:
+                unpacked[i] = unpacked[i].decode('utf-8').strip('\x00')
+
+        return unpacked
 
     def get_key(self, lista):
-        return lista[self.key_index]
+        return lista[self.keyIndex]
 
 class BucketType:
     """
@@ -117,30 +129,30 @@ class ExtensibleHash:
         self.data_file = name_data_file
         self.global_depth = global_depth
         self.max_records = max_records
-        self.buckets = 2
-        self.header = 2
+        self.header = 3
         self.header_size = 4 * self.header
         self.hashindex = 2 ** global_depth
         self.hashindex_size = 4 * self.hashindex
 
-        self.RT = RegistroType(1)
+        self.RT = RegistroType(table_format,1)
         self.BT = BucketType(max_records)
         self._initialize_files(global_depth, force=True)
 
 
-    def _read_header_index(self, file):
+    def _read_header_index(self, file, index):
         """
         Lee el encabezado del archivo de índice y devuelve una tupla
         """
-        data = file.read(self.header_size)
-        return struct.unpack('i' * self.header, data)
+        file.seek(4 * index)
+        data = file.read(4)
+        return struct.unpack('i', data)[0]
 
-    def _write_header_index(self, file, args):
+    def _write_header_index(self, file, value, index):
         """
         Escribe en el encabezado del archivo de índice los elementos de args
         """
-        file.seek(0)
-        file.write(struct.pack('i' * self.header, *args))
+        file.seek(4 * index)
+        file.write(struct.pack('i', value))
 
     def _initialize_files(self, global_depth, force=False):
         """
@@ -150,7 +162,8 @@ class ExtensibleHash:
             with open(self.index_file, 'wb') as f:
                 f.write(struct.pack('i', global_depth))
                 f.write(struct.pack('i', 0))
-                for i in range(2 ** global_depth):
+                f.write(struct.pack('i', 2))
+                for i in range(self.hashindex):
                     if i % 2 == 0:
                         f.write(struct.pack('i', 0))
                     else:
@@ -170,7 +183,7 @@ class ExtensibleHash:
         """
         offset = position * self.RT.size
         data_file.seek(offset)
-        data_file.write(self.RT.to_bytes(*record))
+        data_file.write(self.RT.to_bytes(record))
 
     def _insert_value_in_bucket(self, index_file, bucket_position, register_position):
         """
@@ -226,8 +239,8 @@ class ExtensibleHash:
             old_bucket = self.BT.from_bytes(index_file.read(self.BT.size))
             # crear nuevos buckets
             bucket1_pos = bucket_position
-            bucket2_pos = self.buckets
-            self.buckets += 1
+            bucket2_pos = self._read_header_index(index_file,2)
+            self._write_header_index(index_file, bucket2_pos + 1, 2)
             emptyBucket = {'records': [-1, -1, -1],
                            'local_depth': old_bucket['local_depth'] + 1,
                            'fullness': 0,
@@ -262,11 +275,10 @@ class ExtensibleHash:
 
 
     def _append_record(self, index_file, data_file, record):
-        _, size = self._read_header_index(index_file)
+        size = self._read_header_index(index_file, 1)
         pos = size
-        self._write_record(data_file, pos, record)
-        size += 1
-        self._write_header_index(index_file, [self.global_depth, size])
+        self._write_record(data_file, size, record)
+        self._write_header_index(index_file, size + 1, 1)
         return pos
 
     def imprimir(self):
@@ -282,10 +294,15 @@ class ExtensibleHash:
             print(f"Size: {size}")
 
             # Iterar buckets
-            for i in range(self.buckets):
+            for i in range(self._read_header_index(index_file, 2)):
                 index_file.seek(self.header_size + self.hashindex_size + i * self.BT.size)
                 bucket = self.BT.from_bytes(index_file.read(self.BT.size))
                 print(f"Bucket {i}: {bucket['records']}, fullness: {bucket['fullness']}, local depth: {bucket['local_depth']}")
+                # Iterar registros
+                for j in range(bucket['fullness']):
+                    data_file.seek(bucket['records'][j] * self.RT.size)
+                    registro = self.RT.from_bytes(data_file.read(self.RT.size))
+                    print(f"Registro {j}: {registro}")
 
 
 
@@ -438,13 +455,32 @@ class ExtensibleHash:
 
 
 
-table_format = {"nombre":"10s", "apellido":"20s", "edad": "i", "ciudad": "25s"}
-index_key = 2
+table_format = {"nombre":"10s", "edad": "i", "ciudad": "25s"}
+index_key = 1
 eh = ExtensibleHash(table_format, index_key)
 
-eh.insert(["033", "g", "02"])
-eh.insert(["001", "a", "01"])
-eh.insert(["023", "e", "05"])
-eh.insert(["033", "c", "02"])
+print(''.join(table_format.values()))
 
-eh.imprimir()
+
+while True:
+    print("\nSeleccione una opción:")
+    print("1. Insertar elementos")
+    print("2. Imprimir elementos")
+    print("3. Salir")
+
+    choice = input("Ingrese su opción: ")
+
+    if choice == '1':
+        # Insertar elementos
+        elements = input("Ingrese los elementos a insertar (separados por comas, ej. 033, g, 02): ")
+        elements = elements.split(",")
+        eh.insert([elem.strip() for elem in elements])
+    elif choice == '2':
+        # Imprimir elementos
+        eh.imprimir()
+    elif choice == '3':
+        # Salir del programa
+        print("¡Adiós!")
+        break
+    else:
+        print("Opción inválida. Intente de nuevo.")
