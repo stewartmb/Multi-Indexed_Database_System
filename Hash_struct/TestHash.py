9513f80910dc7a333370616c25fab6ff4d62a5c3
RodrigoLiC <rodrigo.li@utec.edu.pe>
2025-05-05 16:25:25 -0500

    Range Search

--- Hash_struct/TestHash.py
+++ Hash_struct/TestHash.py
@@ -5,31 +5,28 @@ from Hash_struct.Hash import Hash
 class TestHash(unittest.TestCase):
     def setUp(self):
         self.table_format = {"nombre":"12s", "edad": "i", "altura": "d"}
-        self.hash_table = Hash(self.table_format, "edad")
-        self.K = 200
+        self.hash = Hash(self.table_format, "edad")
+        self.K = 100
         nums = random.sample(range(100, 1000), self.K)
         self.placed = nums[0:self.K // 2]
         self.not_placed = nums[self.K // 2:self.K]
 
-    def test_insert(self):
-
-
+    def test_insert_search(self):
         for i in range(self.K//2):
-            self.hash_table.insert(["NOMBRE", self.placed[i], 1.75])
+            self.hash.insert(["NOMBRE", self.placed[i], 1.75])
 
         # Search for the inserted records
         for i in range(self.K//2):
-            if self.hash_table.search(self.placed[i]) is not None:
-                self.assertEqual(self.hash_table.search(self.placed[i])[1], self.placed[i])
+            if self.hash.search(self.placed[i]) is not None:
+                self.assertEqual(self.hash.search(self.placed[i])[1], self.placed[i])
 
         # Search for records that were not inserted
         for i in range(self.K//2):
-            self.assertFalse(self.hash_table.search(self.not_placed[i]))
+            self.assertEqual(self.hash.search(self.not_placed[i]), None)
 
-    def test_search(self):
-        # Insert a record
-        self.hash_table.insert(["NOMBRE", 25, 1.75])
+    def test_range_search(self):
+        for i in range(80, 215):
+            self.hash.insert(["NOMBRE", i, 1.75])
 
-        for i in range(self.K//2):
-            self.hash_table.insert(["NOMBRE", self.placed[i], 1.75])
-        self.hash_table.search(25)
+        # Search for a range of records
+        print(self.hash.range_search(100, 200))
